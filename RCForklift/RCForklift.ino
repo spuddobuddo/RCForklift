273ee4ae9a3ce72690907c36cb565b61e2a63f26
spuddobuddo <tristanrojo7@gmail.com>
2024-04-08 07:59:04 -0700

    updated arduino part with servo code

--- RCForklift/RCForklift.ino
+++ RCForklift/RCForklift.ino
@@ -4,6 +4,10 @@ int enA = 11;
 int in1 = 12;
 int in2 = 13;
 
+int enB = 8;
+int in3 = 10;
+int in4 = 9;
+
 Servo pootis;
 int pos = 0;
 
@@ -14,24 +18,25 @@ void setup() {
   Serial.begin(9600);
 
   pinMode(enA, OUTPUT);
-	//pinMode(enB, OUTPUT);
+	pinMode(enB, OUTPUT);
+
 	pinMode(in1, OUTPUT);
 	pinMode(in2, OUTPUT);
-	//pinMode(in3, OUTPUT);
-	//pinMode(in4, OUTPUT);
+
+	pinMode(in3, OUTPUT);
+	pinMode(in4, OUTPUT);
 
   digitalWrite(in1, LOW);
 	digitalWrite(in2, LOW);
-	//digitalWrite(in3, LOW);
-	//digitalWrite(in4, LOW);
+  digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
 
-  //pootis.attach(10);
+  pootis.attach(7);
 }
 
 void loop() {
   analogWrite(enA, 255);
-  // digitalWrite(in1, HIGH);
-  // digitalWrite(in2, LOW);
+  analogWrite(enB, 225);
   
   if(Serial.available())
   {
@@ -41,22 +46,37 @@ void loop() {
   {
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
   }
   else if(c == '2')
   {
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
   }
   else if(c == '0')
   {
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+  }
+  else if(c == '3')
+  {
+    pootis.write(180);
+  }
+  else if(c == '4')
+  {
+    pootis.write(0);
+  }
+  else if(c == '5')
+  {
+    pootis.write(90);
   }
 
-  // int buttonState = digitalRead(in1);
-  // Serial.println();
-  // delay(500);
-  
+    // Testing servo:
 //   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
 //     // in steps of 1 degree
 //     pootis.write(pos);              // tell servo to go to position in variable 'pos'
@@ -67,7 +87,7 @@ void loop() {
 //     delay(15);                       // waits 15ms for the servo to reach the position
 //   }
 
-//   // put your main code here, to run repeatedly:
+//  // Testing motors:
 //  // Set motors to maximum speed
 // 	// For PWM maximum possible values are 0 to 255
 // 	analogWrite(enA, 255);
@@ -80,7 +100,7 @@ void loop() {
 // 	// digitalWrite(in4, LOW);
 // 	delay(2000);
 	
-// 	// Now change motor directions
+// 	//Change motor directions
 // 	digitalWrite(in1, LOW);
 // 	digitalWrite(in2, HIGH);
 // 	// digitalWrite(in3, LOW);
